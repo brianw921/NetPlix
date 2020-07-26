5bc70acc28eb483114a8297def0897ac109a1473
Brian Wong <bwong92@gmail.com>
2020-07-25 20:39:54 -0400

    Axios setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import {instance} from './axios';
+import Row from 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hey Brian {process.env.REACT_APP_HELLO}</h1>
+      <Row title="NetFlix Originals"/>
+      <Row title="Trending Now" />
     </div>
   );
 }
