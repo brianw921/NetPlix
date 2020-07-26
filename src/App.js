2bd026d267aa7d25d271de8f49de99292521fde2
Brian Wong <bwong92@gmail.com>
2020-07-25 22:05:21 -0400

    Set up hooks+context api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-
 import Row from './Components/Row.js'
 import './App.css';
+//fetch URL's from request.js
+import requests from './requests.js';
 
 function App() {
   return (
     <div className="App">
       <h1>Hey Brian {process.env.REACT_APP_HELLO}</h1>
-      <Row title="NetFlix Originals"/>
-      <Row title="Trending Now" />
+      <Row title="NetFlix Originals" fetchURL={requests.fetchNetflixOriginals}/>
+      <Row title="Trending Now" fetchURL={requests.fetchTrending}/>
     </div>
   );
 }
--- src/Components/Row.js
+++ src/Components/Row.js
@@ -2,8 +2,8 @@ import React, { useState }from 'react'
 
 function Row({title}) {
     const [movies, setMovies] = useState([]);
-
-    
+    console.log("hi", title)
+    //context API
     return (
         <div>
             <h2>{title}</h2>
