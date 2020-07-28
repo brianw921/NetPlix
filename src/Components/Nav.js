6e726e81e7ecd4581db5dd57b834219474d71bab
Brian Wong <bwong92@gmail.com>
2020-07-28 16:17:55 -0400

    navbar

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import "./Nav.css"
 
 function Nav() {
+    const [ show, handleShow ] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100){
+                handleShow(true);
+            } else {
+                handleShow(false);
+            }
+        });
+        return () => {
+            window.removeEventListener("scroll")
+        }
+    },[])
     return (
-        <div className="nav">
+        <div className={`nav ${show && "nav-black"}`}>
             <img
                className = "nav-logo"
                src = "https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png"
