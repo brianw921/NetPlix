1453c888f0e3378b08a4638331aa577e45905566
Brian Wong <bwong92@gmail.com>
2020-07-28 15:29:00 -0400

    nav bar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import Row from './Components/Row.js'
+import Row from './Components/Row.js';
 import './App.css';
 //fetch URL's from request.js
 import requests from './requests.js';
 import Banner from './Components/Banner.js';
+import Nav from './Components/Nav.js';
 
 function App() {
   console.log(requests)
   return (
     <div className="App">
       {/* Nav */}
-      
+      <Nav/>
       <Banner/>
       <Row title="Netplix Originals" 
       fetchUrl={requests.fetchNetflixOriginals}
